c34afda3300d996778d6c936143f64c44f2b9786
africanogerson <48342512+africanogerson@users.noreply.github.com>
2022-09-05 13:29:12 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -6,13 +6,13 @@ SCORES_FOLDER='results'
 
 export PYTHONPATH=$(pwd):$PYTHONPATH
 
-#echo 'Stage 4: Train DNN'
-#python3 train.py -v \
-#    --config $CONFIG \
-#    --use_gpu
-
-echo 'Stage 5: Final scores'
-python3 test.py -v \
+echo 'Stage 4: Train DNN'
+python3 train.py -v \
     --config $CONFIG \
-    --use_gpu \
-    --scores_folder $SCORES_FOLDER
+    --use_gpu
+
+#echo 'Stage 5: Final scores'
+#python3 test.py -v \
+#    --config $CONFIG \
+#    --use_gpu \
+#    --scores_folder $SCORES_FOLDER
